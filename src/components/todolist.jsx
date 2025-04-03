353602db2cf1c6b15b7687defcc5b6bce9703bc3
desireeshortiee <desireeganohay13@gmail.com>
2025-04-04 01:11:29 +0800

    wewewe

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -12,7 +12,7 @@ export default function TodoList() {
     return localStorage.getItem("theme") === "dark";
   });
 
-  const API_URL = "http://127.0.0.1:8000/mytodo/";
+  const API_URL = "https://backendmaria-d9eg.onrender.com/mytodo/";
 
   useEffect(() => {
     document.body.className = darkMode ? "dark" : "";
